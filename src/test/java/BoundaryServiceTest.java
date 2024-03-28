42ebb4103c9d2541bfbdfa510a60f5af8b608ee3
Денис <puteec228@gmail.com>
2024-03-28 20:20:44 +0300

    билд, упавший из-за недостаточного покрытия

--- src/test/java/BoundaryServiceTest.java
+++ src/test/java/BoundaryServiceTest.java
@@ -14,18 +14,18 @@ public class BoundaryServiceTest {
     private final BoundaryService boundaryService = new BoundaryService();
 
 //    private static Stream<int[]> arrays() {
-//        return Stream.of((new int[]{3, 15, 0}), (new int[]{6,4, 3}), (new int[]{88, 3, -1}), (new int[]{3, 2, 1}));
+//        return Stream.of((new int[]{3, 15, 0}), (new int[]{6,4, 3}), (new int[]{88, 3, -1}), (new int[]{3, 2, 12}));
 //    }
-
-    @ParameterizedTest(name = "Array test  {0}")
-    @DisplayName("Finding the minimum element in an array")
-    @Description("Minimum array element found")
-    @MethodSource("arrays")
-    public void findMinElementInArray(int[] arr) {
-        OptionalInt min = OptionalInt.of(boundaryService.findMin(arr));
-        OptionalInt exp = stream(arr).min();
-        assertEquals(exp, min);
-
-    }
+//
+//    @ParameterizedTest(name = "Array test  {0}")
+//    @DisplayName("Finding the minimum element in an array")
+//    @Description("Minimum array element found")
+//    @MethodSource("arrays")
+//    public void findMinElementInArray(int[] arr) {
+//        OptionalInt min = OptionalInt.of(boundaryService.findMin(arr));
+//        OptionalInt exp = stream(arr).min();
+//        assertEquals(exp, min);
+//
+//    }
 
 }
